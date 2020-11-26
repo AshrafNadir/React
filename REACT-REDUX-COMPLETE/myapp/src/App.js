0e4801d7114537e2949a42d8b207737102228b2f
unknown <nadir.tafadar@gmail.com>
2020-11-26 20:25:43 +0600

    new file added

--- REACT-REDUX-COMPLETE/myapp/src/App.js
+++ REACT-REDUX-COMPLETE/myapp/src/App.js
@@ -1,8 +1,20 @@
+import React, { Component } from "react";
+import Ninjas from "./Ninjas";
+
 function App() {
     return ( <
         div className = "App" >
         <
-        h1 > My first React App < /h1>{" "} <
+        h1 > My first React App < /h1> <p> Welcome: ) </p >
+        <
+        Ninjas name = "Ryu"
+        age = "25"
+        belt = "black" / >
+        <
+        Ninjas name = "Nadir"
+        age = "25"
+        belt = "white" / >
+        <
         /div>
     );
 }
