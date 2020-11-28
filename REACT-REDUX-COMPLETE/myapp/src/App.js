5f700bff33d7d5c22b9618cdf1e9500d2382285b
unknown <nadir.tafadar@gmail.com>
2020-11-28 12:27:16 +0600

    worked on ninjas.js

--- REACT-REDUX-COMPLETE/myapp/src/App.js
+++ REACT-REDUX-COMPLETE/myapp/src/App.js
@@ -1,22 +1,26 @@
+import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import Ninjas from "./Ninjas";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > My first React App < /h1> <p> Welcome: ) </p >
-        <
-        Ninjas name = "Ryu"
-        age = "25"
-        belt = "black" / >
-        <
-        Ninjas name = "Nadir"
-        age = "25"
-        belt = "white" / >
-        <
+    state = {
+        ninjas: [
+            { Name: "Nadir", Age: 26, Belt: "Black", ID: 1 },
+            { Name: "Adil", Age: 27, Belt: "Yellow", ID: 2 },
+            { Name: "Shakhawat", Age: 28, Belt: "White", ID: 3 },
+        ],
+    };
+    render(); {
+        return ( <
+                div className = "App" >
+                <
+                h1 > My first React App < /h1> <
+                p > Welcome: ) < /p> <
+            Ninjas ninjas = { this.state.ninjas }
+        /> <
         /div>
     );
+}
 }
 
 export default App;
