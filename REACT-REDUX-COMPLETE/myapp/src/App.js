125e6c01dfe4625a63ad34ac457d8e15a11cf98d
unknown <nadir.tafadar@gmail.com>
2020-12-03 12:52:07 +0600

    committed

--- REACT-REDUX-COMPLETE/myapp/src/App.js
+++ REACT-REDUX-COMPLETE/myapp/src/App.js
@@ -3,24 +3,23 @@ import React, { Component } from "react";
 import Ninjas from "./Ninjas";
 
 function App() {
-    state = {
-        ninjas: [
-            { Name: "Nadir", Age: 26, Belt: "Black", ID: 1 },
-            { Name: "Adil", Age: 27, Belt: "Yellow", ID: 2 },
-            { Name: "Shakhawat", Age: 28, Belt: "White", ID: 3 },
-        ],
-    };
+    /*  state = {
+            ninjas: [
+                { Name: "Nadir", Age: 26, Belt: "Black", ID: 1 },
+                { Name: "Adil", Age: 27, Belt: "Yellow", ID: 2 },
+                { Name: "Shakhawat", Age: 28, Belt: "White", ID: 3 },
+            ],
+        };*/
     render(); {
         return ( <
-                div className = "App" >
-                <
-                h1 > My first React App < /h1> <
-                p > Welcome: ) < /p> <
+            div className = "App" >
+            <
+            h1 > My first React App < /h1> <p> Welcome: ) </p > { " " } <
             Ninjas ninjas = { this.state.ninjas }
-        /> <
-        /div>
-    );
-}
+            />{" "} <
+            /div>
+        );
+    }
 }
 
 export default App;
--- REACT-REDUX-COMPLETE/myapp/src/Ninjas.js
+++ REACT-REDUX-COMPLETE/myapp/src/Ninjas.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-class Ninjas extends Component {
-    render() {
+import React from "react";
+const Ninjas = ({ ninjas }) => {
         //console.log(this.props);
-        const { ninjas } = this.props;
+        const { ninjas } = props;
         const ninjaList = ninjas.map((ninja) => {
-            return ( <
-                div className = "ninja"
-                key = { ninja.id } >
-                <
-                div > Name: { ninja.name } < /div> <div> Age: {ninja.age} </div > { " " } <
-                div > Belt: { ninja.belt } < /div>{" "} < /
-                div >
-            );
-        });
+                return ( <
+                    div className = "ninja"
+                    key = { ninja.id } >
+                    <
+                    div > Name: { ninja.name } < /div> <div> Age: {ninja.age} </div > < div > Belt: { ninja.belt } < /div> < /
+                    div >
+                )
 
-        return <div className = "ninja-list" > { ninjaList } < /div>;
-    }
-}
-export default Ninjas;
+                return <div className = "ninja-list" > { ninjaList } < /div>;
+            }
+
+
+        }
+
+        export default Ninjas;
