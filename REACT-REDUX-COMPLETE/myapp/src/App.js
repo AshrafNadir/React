5023e20389c55ad5953d2d8774286c792c41b292
unknown <nadir.tafadar@gmail.com>
2020-12-08 20:46:52 +0600

    new file added and worked on forms

--- REACT-REDUX-COMPLETE/myapp/src/App.js
+++ REACT-REDUX-COMPLETE/myapp/src/App.js
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+//import { render } from "@testing-library/react";
 import React, { Component } from "react";
-import Ninjas from "./Ninjas";
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
+
 
 class App extends Component {
     state = {
@@ -10,12 +12,20 @@ class App extends Component {
             { name: 'Shakhawat', age: 28, belt: 'White', id: 3 }
         ]
     }
+    addNinja = (ninja) => {
+        ninja.id = Math.random();
+        let ninjas = [...this.state.ninjas, ninja]
+        this.setState({
+            ninjas: ninjas
+        })
+    }
     render() {
         return (
             <div className="App" >
                 <h1> My first React App </h1>
                 <p> Welcome :)</p>
                 <Ninjas ninjas={this.state.ninjas} />
+                <AddNinja AddNinja={this.AddNinja} />
             </div>
         );
     }
