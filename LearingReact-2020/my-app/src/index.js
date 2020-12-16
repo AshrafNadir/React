39dfae257910d029d2952f10d72a209f24bb73db
unknown <nadir.tafadar@gmail.com>
2020-12-16 19:57:35 +0600

    done

--- LearingReact-2020/my-app/src/index.js
+++ LearingReact-2020/my-app/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-const TodoTitle = "Todo App";
+//const TodoTitle = "Todo App";
 const Desc = "loren ipsum ...";
 const date = "21/2020";
 
+// const headingStyle = {
+//     backgroundColor : "purple",
+//     color: "white",
+//     textAlign : "center",
+//     padding :"15px"
+// }
+
 ReactDOM.render(
     <div>
-        "Welcome !!!"
-         <h1>{TodoTitle}</h1>
+        <h1 className="headingStyle">Todo App</h1>
         <p>{Desc}{date}</p>
     </div>,
     document.getElementById('root')
