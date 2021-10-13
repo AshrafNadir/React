a8e0a7b8f5d49b3b1aeba3953df14ddc8eaf8460
AshrafNadir <nadir.tafadar@gmail.com>
2021-10-13 12:43:11 +0600

    committed

---
+++ ice-cream/src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+      
+            <footer>
+        <div className="{['conatainer', classes.container].join(' ')}">
+          <div>
+            Copyright &copy;2020.
+          </div>
+          <div class="textRight">
+            Built with <span class="red">&hearts;</span> by
+            <a
+              href="https://reactjs.org/docs/create-a-new-react-app.html"
+              target="_blank"
+              >Create React App</a
+            >
+          </div>
+        </div>
+      </footer>
+
+    )
+}
