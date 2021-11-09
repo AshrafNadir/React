37a72326565267ea0b9f0e1a414259a52f764f40
AshrafNadir <nadir.tafadar@gmail.com>
2021-11-09 13:15:03 +0600

    done

--- ice-cream/src/components/Builder/Builder.jsx
+++ ice-cream/src/components/Builder/Builder.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import classes from './Builder.module.css'
+import Items from './Itmes/Items'
+import TotalPrice from './TotalPrice/TotalPrice'
 
 export const Builder = () => {
     return (
         <div className={classes.builder}>
               <h3>Build your own Ice Cream Sundae</h3>
-              {/*itmes*/}
+              <Items />
 
-              {/*total Price*/}
+              <TotalPrice />
 
               <button type="button" className={[classes.order,'rounder'].join(' ')}>
                 Add to Cart
---
+++ ice-cream/src/components/Builder/Itmes/Items.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from '../Itmes/Item'
+
+export const Items = () => {
+    return (
+        <div>
+            <ul>
+                <Item />
+            </ul>
+        </div>
+    )
+}
