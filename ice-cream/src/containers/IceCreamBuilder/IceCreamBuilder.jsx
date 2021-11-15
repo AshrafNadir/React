a7f60f4081d477761204c78b70d10a7e9f2d901c
AshrafNadir <nadir.tafadar@gmail.com>
2021-11-15 22:05:43 +0600

    ....

--- ice-cream/src/containers/IceCreamBuilder/IceCreamBuilder.jsx
+++ ice-cream/src/containers/IceCreamBuilder/IceCreamBuilder.jsx
@@ -4,11 +4,28 @@ import { IceCream } from '../../components/IceCream/IceCream';
 import classes from './IceCreamBuilder.module.css';
 
 export default class IceCreamBuilder extends Component{
-    state={};
+    state={
+        items:{
+            vanilla:45,
+            chocolate:50,
+            lemon:30,
+            orange:70,
+            strawberry:80,
+
+
+
+        },
+      scoops: [],
+      totalPrice :0,
+
+        
+    };
+
     render(){
+        const{items}=this.state;
         return (
             <div className={[classes.container,'container'].join(' ')}>
-               <IceCream />
+               <IceCream items={items}/>
                <Builder />
                 
             </div>
