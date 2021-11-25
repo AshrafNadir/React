b52c32597d65d70b376a985db6e93efb34a56807
AshrafNadir <nadir.tafadar@gmail.com>
2021-11-25 13:25:20 +0600

    .....

--- ice-cream/src/components/IceCream/IceCream.jsx
+++ ice-cream/src/components/IceCream/IceCream.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import classes from './IceCream.module.css';
 import { Scoop } from './Scoop/Scoop';
 
-export const IceCream = () => {
+export const IceCream = ({items}) => {
+  const flavors=Object.keys(items);
     return (
         <div>
             <div className={classes.icecream }>
               <p className={classes.cone}></p>
-             { /* <p>Please start adding scoops</p> */}
-             {/* scoop component*/ }
-             <Scoop />
+              
+             {flavors.map((flavor)=>(
+              <Scoop key={flavor} scoop={flavor}/>
+              ))}
             
               <div className={classes.cherry}></div>
             </div>
